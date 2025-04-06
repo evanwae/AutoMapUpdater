acceff9aa3bcb13281b3485938ee8bda0bae12a3
Evan Veenboer <evanveenboer@Evans-MacBook-Pro-7.local>
2025-04-06 09:26:24 -0400

    Initial commit of AutoMapUpdater plugin with updated Java 17 configuration

---
+++ src/main/java/evanwae/automapupdater/AutoMapUpdater.java
@@ -0,0 +1,13 @@
+package evanwae.automapupdater;
+
+/**
+ * Hello world!
+ *
+ */
+public class AutoMapUpdater
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
